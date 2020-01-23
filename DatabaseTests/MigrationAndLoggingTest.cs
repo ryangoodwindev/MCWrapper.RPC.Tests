3f3ceb7083266d9859d444a0989693f8869e9dc5
scanlifter <ryan@ryangoodwin.dev>
2020-01-23 01:21:22 -0700

    Myndblock.MultiChain.Database tests passing

---
+++ DatabaseTests/MigrationAndLoggingTest.cs
@@ -0,0 +1,93 @@
+﻿using MCWrapper.Ledger.Entities;
+using MCWrapper.Ledger.Entities.Extensions;
+using MCWrapper.RPC.Connection;
+using MCWrapper.RPC.Ledger.Clients;
+using MCWrapper.RPC.Test.ServicesPipeline;
+using Myndblock.MultiChain.Database;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MCWrapper.RPC.Tests.DatabaseTests
+{
+    [TestFixture]
+    public class MigrationAndLoggingTest
+    {
+        /*
+         
+            Please note: 
+
+            There are two types of methods demonstrated below for each test.
+
+            Explicit method => Requires that the target blockchain's name must be passed as an argument to the
+                               associated method.
+
+            Inferred method => The target blockchain's name is not required to be passed as an agrument directly,
+                               however, these methods do require that the RpcOptions have been configured properly
+                               during application startup.
+            
+            All variables beginning with the 'exp' prefix are the result of an explicit method.
+            All variables beginning with the 'inf' prefix are the result of an inferred method.
+             
+        */
+
+        // Inject services
+        private readonly IMultiChainRpcWallet _wallet;
+        private readonly ITransactionRepo _contract;
+        private readonly string _chainName;
+        private readonly string _address;
+
+        // Use mock startup service container
+        private readonly ExplicitStartupWithDatabase _services = new ExplicitStartupWithDatabase();
+
+        // Create a new RpcControlClientTests instance
+        public MigrationAndLoggingTest()
+        {
+            _wallet = _services.GetRequiredService<IMultiChainRpcWallet>();
+            _contract = _services.GetRequiredService<ITransactionRepo>();
+            _address = _wallet.RpcOptions.ChainAdminAddress;
+            _chainName = _wallet.RpcOptions.ChainName;
+        }
+
+        [Test]
+        public async Task TransactionLogTestAsync()
+        {
+            /*
+               Explicit blockchain name test
+            */
+
+            // Act - Ask network for blockchain params
+            var exp = await _wallet.IssueAsync(_chainName, UUID.NoHyphens, _address, new AssetEntity(), 100, 1, 0, new Dictionary<string, string>() { { "text", "Test data text".ToHex() } });
+            var transaction_0 = NewTransaction(_chainName, nameof(_wallet.IssueAsync), txid: exp.Result);
+
+            // Assert
+            Assert.DoesNotThrowAsync(async () => await _contract.CreateAsync(transaction_0));
+            Assert.IsTrue(exp.IsSuccess());
+            Assert.IsInstanceOf<RpcResponse<string>>(exp);
+            Assert.IsTrue(transaction_0.Id.HasValue);
+
+            /*
+               Inferred blockchain name test
+            */
+
+            // Act - Ask network for blockchain params
+            var inf = await _wallet.IssueAsync(_address, new AssetEntity(), 100, 1, 0, new Dictionary<string, string>() { { "text", "Test data text".ToHex() } });
+            var transaction_1 = NewTransaction(_chainName, nameof(_wallet.IssueAsync), txid: inf.Result);
+
+            // Assert
+            Assert.DoesNotThrowAsync(async () => await _contract.CreateAsync(transaction_1));
+            Assert.IsTrue(inf.IsSuccess());
+            Assert.IsInstanceOf<RpcResponse<string>>(inf);
+            Assert.IsTrue(transaction_1.Id.HasValue);
+        }
+
+        private TransactionModel NewTransaction(string blockchain, string targetMethod, string txid)
+        {
+            return new TransactionModel(blockchain, targetMethod, txid)
+            {
+                CreatedBy = nameof(TransactionLogTestAsync),
+                LastModifiedBy = nameof(TransactionLogTestAsync)
+            };
+        }
+    }
+}
--- ServicesPipeline/MockStartup.cs
+++ ServicesPipeline/MockStartup.cs
@@ -1,6 +1,7 @@
 ﻿using MCWrapper.RPC.Extensions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Myndblock.MultiChain.Database;
 
 namespace MCWrapper.RPC.Test.ServicesPipeline
 {
@@ -38,6 +39,48 @@ namespace MCWrapper.RPC.Test.ServicesPipeline
         }
     }
 
+    public class ExplicitStartupWithDatabase : MockStartup
+    {
+        private const string TestConnectionString = "Data Source=localhost;Initial Catalog=MultiChainDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        /// <summary>
+        /// 
+        /// ExplicitMockServices adds MultiChain RPC services to the test bank's
+        /// service container using the explicit .AddMultiChainCoreRpcServices(RpcOptions, RuntimeParamOptions) 
+        /// extension method available from the MCWrapper.RPC.Extensions namespace.
+        /// 
+        /// <para>
+        ///     The explicit extension method will cause MCWrapper.RPC to load the
+        ///     RpcOptions configuration values explicitly from the passed Action
+        ///     RpcOptions parameters.
+        /// </para>
+        /// <para>Your explicit MultiChain network parameters will differ from the example values listed below.</para>
+        /// 
+        /// </summary>
+        public ExplicitStartupWithDatabase()
+        {
+            Services.AddMultiChainCoreRpcServices(rpcOptions =>
+            {
+                rpcOptions.ChainHostname = "localhost";
+                rpcOptions.ChainRpcPort = 8384;
+                rpcOptions.ChainUseSsl = true;
+
+                rpcOptions.ChainName = "CurrencyTestCoin";
+                rpcOptions.ChainUsername = "multichainrpc";
+                rpcOptions.ChainPassword = "Rfy4Q2Wbf35DE3ZLvUtGYRD8erByPsjd4gKyuNFkWx9";
+
+                rpcOptions.ChainAdminAddress = "15UxmgMF9AM7JcXZKn4JcKutuQ6q7iSNp4RHVg";
+                rpcOptions.ChainBurnAddress = "1XXXXXXXatXXXXXXP9XXXXXXTdXXXXXXXMSFht";
+            })
+                .ConfigureMultiChainDbStorage(options =>
+                {
+                    options.ConnectionString = TestConnectionString;
+                    options.StoragePlatform = StoragePlatform.SqlServer;
+                    options.MigrationMode = MigrationMode.Auto;
+                });
+        }
+    }
+
     public class IConfigurationStartup : MockStartup
     {
         /// <summary>
@@ -62,6 +105,41 @@ namespace MCWrapper.RPC.Test.ServicesPipeline
             IConfiguration configuration = builder.Build();
 
             Services.AddMultiChainCoreRpcServices(configuration, useSecrets: false);
+        }
+    }
+
+    public class IConfigurationStartupWithDatabase : MockStartup
+    {
+        private const string TestConnectionString = "Data Source=localhost;Initial Catalog=MultiChainDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        /// <summary>
+        /// 
+        /// IConfigurationMockServices adds MultiChain RPC services to the test bank's
+        /// service container using the IConfiguration pipeline
+        /// .AddMultiChainCoreRpcServices(IConfiguration configuration, bool useSecrets = false) 
+        /// extension method available from the MCWrapper.RPC.Extensions namespace.
+        /// 
+        /// <para>
+        ///     The IConfiguration extension method will cause MCWrapper.RPC to load 
+        ///     the RpcOptions configuration values from an external json setttings file.
+        ///     Usually appsettings.json.
+        /// </para>
+        /// <para>Support for Secret Manager is attempted but not tested yet.</para>
+        /// </summary>
+        public IConfigurationStartupWithDatabase()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfiguration configuration = builder.Build();
+
+            Services.AddMultiChainCoreRpcServices(configuration, useSecrets: false)
+                .ConfigureMultiChainDbStorage(options =>
+                {
+                    options.ConnectionString = TestConnectionString;
+                    options.StoragePlatform = StoragePlatform.SqlServer;
+                    options.MigrationMode = MigrationMode.Auto;
+                });
         }
     }
 
@@ -92,6 +170,41 @@ namespace MCWrapper.RPC.Test.ServicesPipeline
         }
     }
 
+    public class SecretsManagerStartupWithDatabase : MockStartup
+    {
+        private const string TestConnectionString = "Data Source=localhost;Initial Catalog=MultiChainDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        /// <summary>
+        /// 
+        /// IConfigurationMockServices adds MultiChain RPC services to the test bank's
+        /// service container using the IConfiguration pipeline
+        /// .AddMultiChainCoreRpcServices(IConfiguration configuration, bool useSecrets = false) 
+        /// extension method available from the MCWrapper.RPC.Extensions namespace.
+        /// 
+        /// <para>
+        ///     The IConfiguration extension method will cause MCWrapper.RPC to load 
+        ///     the RpcOptions configuration values from an external json setttings file.
+        ///     Usually appsettings.json.
+        /// </para>
+        /// <para>Support for Secret Manager is attempted but not tested yet.</para>
+        /// </summary>
+        public SecretsManagerStartupWithDatabase()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddUserSecrets("Enter your local secret manager id here or configure otherwise.");
+
+            IConfiguration configuration = builder.Build();
+
+            Services.AddMultiChainCoreRpcServices(configuration, useSecrets: true)
+                .ConfigureMultiChainDbStorage(options =>
+                {
+                    options.ConnectionString = TestConnectionString;
+                    options.StoragePlatform = StoragePlatform.SqlServer;
+                    options.MigrationMode = MigrationMode.Auto;
+                });
+        }
+    }
+
     public class ParameterlessStartup : MockStartup
     {
         /// <summary>
@@ -107,8 +220,33 @@ namespace MCWrapper.RPC.Test.ServicesPipeline
         /// </para>
         /// 
         /// </summary>
-        public ParameterlessStartup() =>
-            Services.AddMultiChainCoreRpcServices();
+        public ParameterlessStartup() => Services.AddMultiChainCoreRpcServices();
+    }
+
+    public class ParameterlessStartupWithDatabase : MockStartup
+    {
+        private const string TestConnectionString = "Data Source=localhost;Initial Catalog=MultiChainDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        /// <summary>
+        /// 
+        /// ParameterlessMockServices adds MultiChain RPC services to the test bank's
+        /// service container using the parameterless .AddMultiChainCoreRpcServices() 
+        /// extension method available from the MCWrapper.RPC.Extensions namespace.
+        /// 
+        /// <para>
+        ///     The parameterless extension method 
+        ///     will cause MCWrapper.RPC to look to the local machine's environment 
+        ///     store for the RpcOptions configuration values.
+        /// </para>
+        /// 
+        /// </summary>
+        public ParameterlessStartupWithDatabase() => Services.AddMultiChainCoreRpcServices()
+            .ConfigureMultiChainDbStorage(options =>
+            {
+                options.ConnectionString = TestConnectionString;
+                options.StoragePlatform = StoragePlatform.SqlServer;
+                options.MigrationMode = MigrationMode.Auto;
+            });
     }
 
     /// <summary>
